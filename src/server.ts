ce06e8d1b46759cc5de3da9d23c5d5ac5353a924
Jorge Oliveira <jorge.oliveira@groupsoftware.com.br>
2022-04-20 12:02:23 -0300

    Directories restructuring

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { categoriesRoutes } from "./routes/categories.routes";
+import { specificationsRoutes } from "./routes/specifications.routes";
 
 const port = 4003;
 const url = `http://localhost:${port}`;
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use("/categories", categoriesRoutes);
+app.use("/specifications", specificationsRoutes);
 
 app.listen(port, () =>
   console.log(`Application is running! Access URL: ${url}`)
