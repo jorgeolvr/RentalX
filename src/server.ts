39a50b72d5e32619ecb5be05d7a35b20420afbe9
Jorge Oliveira <jorge.oliveira@groupsoftware.com.br>
2022-04-26 16:46:51 -0300

    Swagger documentation added

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import { router } from "./routes";
 
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger.json";
+
 const port = 4003;
 const url = `http://localhost:${port}`;
 
@@ -8,6 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(port, () =>
   console.log(`Application is running! Access URL: ${url}`)
